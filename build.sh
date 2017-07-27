694c93c5995aee01fcda58ebdddf4debb2afaf9b
Kang Jianbin <kjbmail@qq.com>
2017-07-27 18:21:41 +0800

    Update build.

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 source env.sh
 
-if [ -z "$LIBCLANG_DIR" ]; then
+if [ -z "$LIBCLANG_HEADER" ]; then
     LIBCLANG_DIR=$(llvm-config --prefix)/lib/clang/$(llvm-config --version)
 fi
 
-go build -ldflags "-X main.ClangHeaderDir=${LIBCLANG_DIR}/include"
+go build -ldflags "-X main.ClangHeaderDir=${LIBCLANG_HEADER}/include"
--- env.sh
+++ env.sh
@@ -1,13 +1,34 @@
-#!/bin/sh
+#!/bin/bash
+
+llvm_config="llvm-config"
+has_llvm_config=0
+hash $llvm_config 2>/dev/null
+if [ $? = "0" ]; then
+    has_llvm_config=1
+fi
+os=$(uname)
+
 
 if [ -z "$LLVM_DIR" ]; then
-    export CGO_CFLAGS=$(llvm-config --cflags)
+    if [[ $has_llvm_config == 0 ]]; then
+        echo "Can't find $llvm_config. Please set environment variable LLVM_DIR"
+        exit 1
+    fi
+    LLVM_DIR=$($llvm_config --prefix)
+    export CGO_CFLAGS=$($llvm_config --cflags)
+    export CGO_LDFLAGS=$($llvm_config --ldflags)
 else
     export CGO_CFLAGS="-I$LLVM_DIR/include"
+    if [ -z "$LLVM_LIB" ]; then
+        if [ "$os" = "Linux" ]; then
+            export CGO_LDFLAGS="-L$LLVM_DIR/lib"
+        else
+            export CGO_LDFLAGS="-L$LLVM_DIR/bin"
+        fi
+    else
+        export CGO_LDFLAGS="-L$LLVM_LIB"
+    fi
 fi
 
-if [ -z "$LLVM_LIB" ]; then
-    export CGO_LDFLAGS=$(llvm-config --ldflags)
-else
-    export CGO_LDFLAGS="-L$LLVM_LIB"
-fi
+clang_header="$LLVM_DIR/lib/clang/*/include"
+[ -e $clang_header ] && export LIBCLANG_HEADER="$clang_header"
